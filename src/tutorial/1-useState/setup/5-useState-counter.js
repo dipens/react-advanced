325089fcd3064dc05df8141046edbeb977ea50ec
Dipen Shah <dshah@trillium.group>
2021-01-15 15:21:28 -0500

    useState Counter

--- src/tutorial/1-useState/setup/5-useState-counter.js
+++ src/tutorial/1-useState/setup/5-useState-counter.js
@@ -1,7 +1,18 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 const UseStateCounter = () => {
-  return <h2>useState counter example</h2>;
+  const [value, setValue] = React.useState(0);
+  return (
+    <>
+    <section style={{margin: '4rem 0'}}>
+      <h2>Regular Counter</h2>
+      <h1>{value}</h1>
+      <button className='btn' onClick={() => {setValue(value+1)}}>Increase</button>
+      <button className='btn' onClick={() => {setValue(value-1)}}>Decrease</button>
+      <button className='btn' onClick={() => {setValue(0)}}>Reset</button>
+    </section>
+    </>
+  )
 };
 
 export default UseStateCounter;
