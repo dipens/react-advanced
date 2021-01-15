53f4902876a54c89d333fea9c887610da0ba3eee
Dipen Shah <dshah@trillium.group>
2021-01-15 16:06:27 -0500

    useEffect cleanup

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import UseStateArray from './tutorial/1-useState/setup/3-useState-array';
 import UseStateOjbect from './tutorial/1-useState/setup/4-useState-object';
 import UseStateCounter from './tutorial/1-useState/setup/5-useState-counter';
 import UseEffectBasics from './tutorial/2-useEffect/setup/1-useEffect-basics';
+import UseEffectCleanUp from './tutorial/2-useEffect/setup/2-useEffect-cleanup';
 
 function App() {
   return (
     <div className='container'>
+      <UseEffectCleanUp />
       <UseEffectBasics />
       <UseStateCounter />
       <UseStateOjbect />
--- src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
+++ src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
@@ -1,10 +1,24 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
 // cleanup function
 // second argument
 
 const UseEffectCleanup = () => {
-  return <h2>useEffect cleanup</h2>;
+  const [size, setSize] = React.useState(window.innerWidth);
+  const checkSize = () => {
+    setSize(window.innerWidth);
+  }
+  React.useEffect(() => {
+    window.addEventListener('resize', checkSize);
+    return () => {
+      window.removeEventListener('resize', checkSize);
+    }
+  });
+  return (
+  <>
+  <h1>Window size</h1>
+  <h1>{size} px</h1>
+  </>);
 };
 
 export default UseEffectCleanup;
