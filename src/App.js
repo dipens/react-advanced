f04d804e4f05caeaa498f09bb1b4e4f3349edb0d
Dipen Shah <dshah@trillium.group>
2021-01-15 15:41:31 -0500

    useEffect setup

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import Setup from './tutorial/1-useState/setup/2-useState-basics';
 import UseStateArray from './tutorial/1-useState/setup/3-useState-array';
 import UseStateOjbect from './tutorial/1-useState/setup/4-useState-object';
 import UseStateCounter from './tutorial/1-useState/setup/5-useState-counter';
+import UseEffectBasics from './tutorial/2-useEffect/setup/1-useEffect-basics';
 
 function App() {
   return (
     <div className='container'>
-      <Setup />
-      <UseStateArray />
-      <UseStateOjbect />
+      <UseEffectBasics />
       <UseStateCounter />
+      <UseStateOjbect />
+      <UseStateArray />
+      <Setup />
     </div>
   )
 }
--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -1,9 +1,13 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 // by default runs after every re-render
 // cleanup function
 // second parameter
 const UseEffectBasics = () => {
-  return <h2>useEffect Basics</h2>;
+  return (
+    <>
+    AAA
+    </>
+  );
 };
 
 export default UseEffectBasics;
