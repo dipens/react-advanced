3afa19455df0ce7ded67f67a333815680e757336
Dipen Shah <dshah@trillium.group>
2021-01-15 15:49:34 -0500

    useEffect basics

--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -3,9 +3,14 @@ import React from 'react';
 // cleanup function
 // second parameter
 const UseEffectBasics = () => {
+  const [value, setValue] = React.useState(0);
+  React.useEffect(() => {
+    document.title = `Counter value: ${value}`
+  });
   return (
     <>
-    AAA
+      <h1>{value}</h1>
+      <button className="btn" onClick={() => {setValue(value+1)}}>Increase</button>
     </>
   );
 };
