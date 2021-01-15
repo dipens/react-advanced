ba60ce599a4a879f906b83bb330bec978800f640
Dipen Shah <dshah@trillium.group>
2021-01-15 15:15:37 -0500

    useState Object

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Setup from './tutorial/1-useState/setup/2-useState-basics';
-import UserStateArray from './tutorial/1-useState/setup//3-useState-array';
+import UseStateArray from './tutorial/1-useState/setup/3-useState-array';
+import UseStateOjbect from './tutorial/1-useState/setup/4-useState-object';
 
 function App() {
   return (
     <div className='container'>
       <Setup />
-      <UserStateArray />
+      <UseStateArray />
+      <UseStateOjbect />
     </div>
   )
 }
--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,18 @@
 import React, { useState } from 'react';
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [person,setPerson] = useState({name: 'Dipen', age: 24, msg:'Random msg'});
+  const changeMessage = () => {
+    setPerson({...person, msg: 'Hello world'});
+  };
+  return (
+    <>
+    <h3>{person.name}</h3>
+    <h3>{person.age}</h3>
+    <h3>{person.msg}</h3>
+    <button className="btn" onClick={changeMessage}>Change Message</button>
+    </>
+  );
 };
 
 export default UseStateObject;
