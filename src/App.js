f8301aeb24265113905f6f55b109e1e037eb86ad
Dipen Shah <dshah@trillium.group>
2021-01-16 11:13:47 -0500

    add githubusers

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import UseStateOjbect from './tutorial/1-useState/setup/4-useState-object';
 import UseStateCounter from './tutorial/1-useState/setup/5-useState-counter';
 import UseEffectBasics from './tutorial/2-useEffect/setup/1-useEffect-basics';
 import UseEffectCleanUp from './tutorial/2-useEffect/setup/2-useEffect-cleanup';
+import UseEffectFetch from './tutorial/2-useEffect/setup/3-useEffect-fetch-data';
 
 function App() {
   return (
     <div className='container'>
+      <UseEffectFetch />
       <UseEffectCleanUp />
       <UseEffectBasics />
       <UseStateCounter />
--- src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
+++ src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
@@ -1,9 +1,34 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
 const url = 'https://api.github.com/users';
 
 const UseEffectFetchData = () => {
-  return <h2>fetch data</h2>;
+  const [users, setUsers] = React.useState([]);
+  const getUsers = async () => {
+    const response = await fetch(url);
+    const usersJson = await response.json();
+    setUsers(usersJson);
+  }
+  React.useEffect(() => {
+    getUsers();
+  },[]);
+  return (
+    <>
+      <h3>Github users</h3>
+      <ul className="users">
+      {
+        users.map((user) => {
+          const {id,login,avatar_url, html_url} = user;
+          return <li key={id}>
+            <img src={avatar_url} alt={login}></img>
+            <div><h4>{login}</h4></div>
+            <a href={html_url} target="_blank" rel="noopener noreferrer" >profile</a>
+          </li>
+        })
+      }
+      </ul>
+    </>
+  );
 };
 
 export default UseEffectFetchData;
