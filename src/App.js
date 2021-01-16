20b1495a69e3d936713c325c7461ac3268db7d26
Dipen Shah <dshah@trillium.group>
2021-01-16 17:54:47 -0500

    Memo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 
 import ReactRouterBasics from './tutorial/11-react-router/setup';
+import UseMemoBasics from './tutorial/12-memo-useMemo-useCallback/setup'
 
 function App() {
   return (
     <div className='container'>
+      <UseMemoBasics />
       <ReactRouterBasics />
     </div>
   )
--- src/tutorial/12-memo-useMemo-useCallback/setup/index.js
+++ src/tutorial/12-memo-useMemo-useCallback/setup/index.js
@@ -10,29 +10,33 @@ const url = 'https://course-api.com/javascript-store-products'
 const Index = () => {
   const { products } = useFetch(url)
   const [count, setCount] = useState(0)
-
+  const [cart,setCart] = useState(0);
+  const addToCart = useCallback(() => {
+    setCart(cart+1);
+  },[cart]);
   return (
     <>
       <h1>Count : {count}</h1>
       <button className='btn' onClick={() => setCount(count + 1)}>
         click me
       </button>
-      <BigList products={products} />
+      <h1 style={{marginTop: '3rem'}}>cart: {cart}</h1>
+      <BigList products={products} addToCart={addToCart} />
     </>
   )
 }
 
-const BigList = ({ products }) => {
+const BigList = React.memo(({ products,addToCart }) => {
   return (
     <section className='products'>
       {products.map((product) => {
-        return <SingleProduct key={product.id} {...product}></SingleProduct>
+        return <SingleProduct key={product.id} {...product} addToCart={addToCart}></SingleProduct>
       })}
     </section>
   )
-}
+});
 
-const SingleProduct = ({ fields }) => {
+const SingleProduct = ({ fields, addToCart }) => {
   let { name, price } = fields
   price = price / 100
   const image = fields.image[0].url
@@ -42,6 +46,7 @@ const SingleProduct = ({ fields }) => {
       <img src={image} alt={name} />
       <h4>{name}</h4>
       <p>${price}</p>
+      <button onClick={addToCart}>Add to Cart</button>
     </article>
   )
 }
